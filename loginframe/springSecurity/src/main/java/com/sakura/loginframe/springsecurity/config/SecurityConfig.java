f9f753d7d55d7877ae5132726fd3f9c2986d7930
sakura <sakuraWen@126.com>
2020-03-31 16:04:55 +0800

    spring security config comment

---
+++ loginframe/springSecurity/src/main/java/com/sakura/loginframe/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.sakura.loginframe.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends SecurityConfigurerAdapter {
+    
+    //默认SpringWeb.xml的配置
+    
+    //认证用户来源
+    
+    //配置springSecurity 相关信息    1.静态资源，资源拦截规则   2.指定自定义页面，认证配置， csrf配置
+    
+}
